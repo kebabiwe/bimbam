caae3a769e67964ccfcd31cbbdbee57ceea46474
kebabiwe <153375118+kebabiwe@users.noreply.github.com>
2023-12-13 15:24:47 +0300

    cuts+textview+mainclass

--- app/src/androidTest/java/com/example/bimbam/MainActivity.java
+++ app/src/androidTest/java/com/example/bimbam/MainActivity.java
@@ -1,4 +1,20 @@
 package com.example.bimbam;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainactivity1);
+
+        // Assuming your ImageView is in the activity_main.xml layout
+        ImageView image = findViewById(R.id.image);
+
+        // Now you can work with the 'image' ImageView
+    }
 }
