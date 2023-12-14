79d861702f9c9ee655a05a935f17ec6e2be46df4
kebabiwe <153375118+kebabiwe@users.noreply.github.com>
2023-12-15 00:56:01 +0300

    forms

---
+++ app/src/main/java/com/example/bimbam/MainActivity_settings.kt
@@ -0,0 +1,11 @@
+package com.example.bimbam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_settings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_settings)
+    }
+}
