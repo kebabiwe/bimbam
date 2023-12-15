33faea191497b0268b060f55a747b5e10ea85307
lutayauliana <tikhanoviiich@mail.ru>
2023-12-15 14:10:40 +0300

    acc

--- app/src/main/java/com/example/bimbam/MainActivity_homePage.kt
+++ app/src/main/java/com/example/bimbam/MainActivity_homePage.kt
@@ -1,11 +1,20 @@
 package com.example.bimbam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
 
 class MainActivity_homePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_home_page)
+
+        val View = findViewById<View>(R.id.profile)
+        View.setOnClickListener {
+            val intent = Intent(this@MainActivity_homePage, MainActivity_you_acc::class.java)
+            startActivity(intent)
+        }
     }
 }
