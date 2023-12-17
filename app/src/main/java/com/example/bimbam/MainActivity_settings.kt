f007c308870eb227c73fda5b9f62639882c39311
lutayauliana <tikhanoviiich@mail.ru>
2023-12-17 17:38:42 +0300

    settings

--- app/src/main/java/com/example/bimbam/MainActivity_settings.kt
+++ app/src/main/java/com/example/bimbam/MainActivity_settings.kt
@@ -1,11 +1,20 @@
 package com.example.bimbam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.RelativeLayout
 
 class MainActivity_settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_settings)
+
+        val RelativeLayout = findViewById<RelativeLayout>(R.id.frame_30)
+        RelativeLayout.setOnClickListener {
+            val intent = Intent(this@MainActivity_settings, MainActivity_about::class.java)
+            startActivity(intent)
+        }
     }
 }
