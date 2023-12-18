8c21bc65e868369b4d4e58fb81f28cc6ba481a29
kebabiwe <153375118+kebabiwe@users.noreply.github.com>
2023-12-18 14:47:37 +0300

    reg child+changes in manifest

--- app/src/main/java/com/example/bimbam/MainActivity_registrationChild.kt
+++ app/src/main/java/com/example/bimbam/MainActivity_registrationChild.kt
@@ -1,32 +1,32 @@
+package com.example.bimbam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RelativeLayout
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import com.example.bimbam.R
 import android.graphics.Typeface
+import android.widget.Spinner
+import android.widget.ArrayAdapter
 class MainActivity_registrationChild : AppCompatActivity() {
-
-    private lateinit var relativeLayout: RelativeLayout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_registration_child)
-
-        // Найдите RelativeLayout в вашем макете
-        relativeLayout = findViewById(R.id.form2)
-
-        // Получите выбранный пол из интента
-        val selectedGender = intent.getStringExtra("selectedGender")
-
-        // Проверьте, существует ли RelativeLayout и выбранный пол не является пустым
-        if (relativeLayout != null && selectedGender != null) {
-            // Создайте TextView и установите его текст в выбранный пол
-            val textView = TextView(this)
-            textView.text = "$selectedGender"
-            textView.textSize = 15f // размер текста в sp
-            textView.setTypeface(Typeface.SANS_SERIF, Typeface.NORMAL) // установить стандартный шрифт с жирным начертанием
-            // Добавьте TextView в RelativeLayout
-            relativeLayout.addView(textView)
+        val signupText = findViewById<TextView>(R.id.title)
+        signupText.setOnClickListener {
+            val intent = Intent(this, MainActivity_registrationChild::class.java)
+            startActivity(intent)
         }
-    }
-}
+        val spinner: Spinner = findViewById(R.id.spinner)
+// Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.sex_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+        }
+}}
