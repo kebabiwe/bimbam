c6d9d006c55d4225c34b6b30aed37e4ebc718624
kebabiwe <153375118+kebabiwe@users.noreply.github.com>
2023-12-21 01:22:15 +0300

    changes

--- app/src/main/java/com/example/bimbam/MainActivity_Login.kt
+++ app/src/main/java/com/example/bimbam/MainActivity_Login.kt
@@ -4,16 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RelativeLayout
+import com.example.bimbam.databinding.ActivityMainLoginBinding
 import com.example.bimbam.databinding.ActivityMainRegistrationAccBinding
 import com.example.bimbam.databinding.ActivityMainRegistrationChildBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity_Login : AppCompatActivity() {
-    private lateinit var binding: ActivityMainRegistrationChildBinding
+    private lateinit var binding: ActivityMainLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainRegistrationChildBinding.inflate(layoutInflater)
+        binding = ActivityMainLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val RelativeLayout = findViewById<RelativeLayout>(R.id.button)
         RelativeLayout.setOnClickListener {
