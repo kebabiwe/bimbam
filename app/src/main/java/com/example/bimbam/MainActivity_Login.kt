4a9ab7004e6c255b2203789e34a36e8550fcde65
kebabiwe <153375118+kebabiwe@users.noreply.github.com>
2023-12-25 23:41:57 +0300

    login fix

--- app/src/main/java/com/example/bimbam/MainActivity_Login.kt
+++ app/src/main/java/com/example/bimbam/MainActivity_Login.kt
@@ -26,19 +26,16 @@ class MainActivity_Login : AppCompatActivity() {
             val login = binding.login.text.toString()
             val pass = binding.password.text.toString()
             if (login.isNotEmpty() && pass.isNotEmpty()) {
-
                 firebaseAuth.signInWithEmailAndPassword(login, pass).addOnCompleteListener {
                     if (it.isSuccessful) {
                         val intent = Intent(this, MainActivity_homePage::class.java)
                         startActivity(intent)
                     } else {
                         Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-
                     }
                 }
             } else {
                 Toast.makeText(this, "Не все поля заполнены", Toast.LENGTH_SHORT).show()
-
             }
         }
     }
