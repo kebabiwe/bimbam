81ba5287a0ad0d03828d6d9ab72379071cf2600e
kebabiwe <153375118+kebabiwe@users.noreply.github.com>
2024-01-10 00:16:57 +0300

    take token from user

--- app/src/main/java/com/example/bimbam/Notification_Service.kt
+++ app/src/main/java/com/example/bimbam/Notification_Service.kt
@@ -1,19 +1,30 @@
 package com.example.bimbam
 
 import android.util.Log
+
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 
 class Notification_Service:FirebaseMessagingService() {
     override fun onNewToken(newToken: String) {
         super.onNewToken(newToken)
-        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
             if (!task.isSuccessful) {
-                return@addOnCompleteListener
+                Log.w("TAG", "Fetching FCM registration token failed", task.exception)
+                return@OnCompleteListener
             }
+
+            // Get new FCM registration token
             val token = task.result
-             Log.e("TAG","Toke -> $token")
-        }
+
+            // Log and toast
+            val msg = "Your token: $token"
+
+            Log.d("TAG", msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        })
 
     }
 
