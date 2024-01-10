368e2f6817d658657e2427578cee63a0e74a39d9
lutayauliana <tikhanoviiich@mail.ru>
2024-01-10 23:19:20 +0300

    button

--- app/src/main/java/com/example/bimbam/MainActivity_startmenu.kt
+++ app/src/main/java/com/example/bimbam/MainActivity_startmenu.kt
@@ -1,8 +1,10 @@
 package com.example.bimbam
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.widget.RelativeLayout
 import android.widget.TextView
 
@@ -20,6 +22,26 @@ class MainActivity_startmenu : AppCompatActivity() {
             val intent = Intent(this@MainActivity_startmenu,MainActivity_registrationAcc::class.java)
             startActivity(intent)
         }
+
+        val myButton: RelativeLayout = findViewById(R.id.button)
+        val myTextView: TextView = findViewById(R.id.some_id)
+
+        myButton.setOnTouchListener { view, motionEvent ->
+            when (motionEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    // Нажатие кнопки: Установить зеленый цвет текста
+                    myTextView.setTextColor(Color.GREEN)
+                }
+                MotionEvent.ACTION_UP -> {
+                    // Отпускание кнопки: Установить белый цвет текста
+                    myTextView.setTextColor(Color.WHITE)
+                }
+            }
+            true
+        }
+
+
+
 
     }
 }
